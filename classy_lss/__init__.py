59d420af7d7140d1aafd2e6664bc69bbf03af39a
Nick Hand <nhand@berkeley.edu>
2016-06-20 17:14:45 -0700

    import everything into the main module

---
+++ classy_lss/__init__.py
@@ -0,0 +1,9 @@
+from .version import version as __version__
+
+from .gcl import Cosmology
+from .gcl import ClassEngine
+from .gcl import ClassParams
+from .gcl import LinearPS
+from .gcl import transfers
+
+from .core import *
