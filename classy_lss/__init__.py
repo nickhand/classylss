001933ce086c2913cda8df3dfc430500629293f8
Nick Hand <nhand@berkeley.edu>
2016-06-21 00:33:22 -0700

    clean up bug in the __init__

--- classy_lss/__init__.py
+++ classy_lss/__init__.py
@@ -1,10 +1,10 @@
 from .version import version as __version__
 
-from .gcl import Cosmology
-from .gcl import ClassEngine
-from .gcl import ClassParams
-from .gcl import transfers
-from .gcl import cltypes
+from gcl import Cosmology
+from gcl import ClassEngine
+from gcl import ClassParams
+from gcl import transfers
+from gcl import cltypes
 
 import power
 import tools
