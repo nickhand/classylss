d0a1914b00e79921aad0a53017d2f970245f1418
Nick Hand <nhand@berkeley.edu>
2016-07-12 15:03:49 -0700

    fixes python 3 errors

--- classylss/__init__.py
+++ classylss/__init__.py
@@ -1,11 +1,11 @@
 from .version import version as __version__
 
-from gcl import Cosmology
-from gcl import ClassEngine
-from gcl import ClassParams 
-from gcl import transfers
-from gcl import cltypes
+from .gcl import Cosmology
+from .gcl import ClassEngine
+from .gcl import ClassParams 
+from .gcl import transfers
+from .gcl import cltypes
 
-import power
-import tools
-import correlation
+from . import power
+from . import tools
+from . import correlation
