ae9180af945646b6d50ace561aefc045fabe2193
Nick Hand <nhand@berkeley.edu>
2017-06-09 21:34:49 -0400

    back to dev 0.1.19.dev0

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ CLASS_VERSION = '2.6.0'
 
 MAJOR = 0
 MINOR = 1
-MICRO = 18
-ISRELEASED = True
+MICRO = 19
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 DISTNAME = 'classylss'
