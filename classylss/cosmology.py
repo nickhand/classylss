5f2044b5e73f1d77ec03940a6718fecc48d5dc5e
Yu Feng <rainwoodman@gmail.com>
2017-08-16 00:36:31 -0700

    use __setstate__ to initialize the object.

--- classylss/cosmology.py
+++ classylss/cosmology.py
@@ -14,6 +14,10 @@ class Cosmology(object):
         'Omega_Lambda' : 'Lambda, 0 if w0 is given',
         gauge : synchronous or newtonian
     """
+
+    # delegate resolve order
+    dro = [Spectra, Perturbs, Primordial, Background, ClassEngine]
+
     def __init__(self,
             h=0.67556,
             T_cmb=2.7255,
@@ -33,53 +37,10 @@ class Cosmology(object):
         args = dict(locals())
         args.pop('self')
         kwargs = args.pop('kwargs')
-
         args.update(kwargs)
 
-        self.args = args
-
-        pars = {
-            "output": "vTk dTk mPk",
-            "extra metric transfer functions": "y",
-            }
-        pars.update(args)
-
-        # remove None's -- use CLASS default
-        for key in list(pars.keys()):
-            if pars[key] is None: pars.pop(key)
-
-        assert gauge in ['synchronous', 'newtonian']
-
-        if 'P_k_max_h/Mpc' not in pars:
-            pars['P_k_max_h/Mpc'] = pars.pop('P_k_max')
-
-        if 'z_max_pk' not in pars:
-            pars['z_max_pk'] = pars.pop('P_z_max')
-
-        for m in m_ncdm:
-            if m == 0:
-                raise ValueError("A zero mass is specified in the massive neutrino list.")
-
-        # from CLASS notes:
-        # one more remark: if you have respectively 1,2,3 massive neutrinos,
-        # if you stick to the default value pm equal to 0.71611, designed to give m/omega of
-        # 93.14 eV, and if you want to use N_ur to get N_eff equal to 3.046 in the early universe,
-        # then you should pass here respectively 2.0328,1.0196,0.00641
-
-        N_ur_table = [3.046, 2.0328, 1.0196, 0.00641]
-
-        if 'N_ur' not in pars:
-            pars['N_ur'] = N_ur_table[len(m_ncdm)]
-
-        pars['N_ncdm'] = len(m_ncdm)
-        self.engine = ClassEngine(pars)
-
-        self.delegates = {
-            ClassEngine: self.engine,
-        }
-
-        # delegate resolve order
-        self.dro = [Spectra, Perturbs, Primordial, Background, ClassEngine]
+        # use set state to de-serialize the object.
+        self.__setstate__((args,))
 
     def __dir__(self):
         r = []
@@ -104,7 +65,49 @@ class Cosmology(object):
 
     def __setstate__(self, state):
         args = state[0]
-        self.__init__(**args)
+
+        # remember args for serialization
+        self.args = args
+
+        pars = {
+            "output": "vTk dTk mPk",
+            "extra metric transfer functions": "y",
+            }
+        pars.update(args)
+
+        # remove None's -- use CLASS default
+        for key in list(pars.keys()):
+            if pars[key] is None: pars.pop(key)
+
+        assert args['gauge'] in ['synchronous', 'newtonian']
+
+        if 'P_k_max_h/Mpc' not in pars:
+            pars['P_k_max_h/Mpc'] = pars.pop('P_k_max')
+
+        if 'z_max_pk' not in pars:
+            pars['z_max_pk'] = pars.pop('P_z_max')
+
+        for m in args['m_ncdm']:
+            if m == 0:
+                raise ValueError("A zero mass is specified in the massive neutrino list.")
+
+        # from CLASS notes:
+        # one more remark: if you have respectively 1,2,3 massive neutrinos,
+        # if you stick to the default value pm equal to 0.71611, designed to give m/omega of
+        # 93.14 eV, and if you want to use N_ur to get N_eff equal to 3.046 in the early universe,
+        # then you should pass here respectively 2.0328,1.0196,0.00641
+
+        N_ur_table = [3.046, 2.0328, 1.0196, 0.00641]
+
+        if 'N_ur' not in pars:
+            pars['N_ur'] = N_ur_table[len(args['m_ncdm'])]
+
+        pars['N_ncdm'] = len(args['m_ncdm'])
+        self.engine = ClassEngine(pars)
+
+        self.delegates = {
+            ClassEngine: self.engine,
+        }
 
     def clone(self, **kwargs):
         """ create a new cosmology based on modification of self """
