d40538dfba72d065728eb32632ada6a9b82c58dc
Nick Hand <nhand@berkeley.edu>
2017-08-21 23:45:48 -0400

    halofit nonlinear class

---
+++ classylss/power/halofit.py
@@ -0,0 +1,24 @@
+import numpy
+
+class HalofitPower(object):
+
+    def __init__(self, cosmo, z):
+
+        # make sure nonlinear is enabled in CLASS
+        if not cosmo.nonlinear:
+            cosmo = cosmo.clone(extra={'non linear':'halofit'})
+        self.cosmo = cosmo
+        self.z = z
+
+    def __call__(self, k):
+
+        k = numpy.asarray(k)
+        kmin = self.cosmo.P_k_min
+        inrange = k > 1.00001*kmin
+
+        Pk = numpy.zeros_like(k)
+        k_in = k[inrange]; k_out = k[~inrange]
+
+        Pk[inrange] = self.cosmo.get_pk(k=k_in, z=self.z)
+        Pk[~inrange] = self.cosmo.get_pklin(k=k_out, z=self.z)
+        return Pk
