51143938d8edbe9e6553cc5f0300e41b78d6e57e
Yu Feng <rainwoodman@gmail.com>
2017-08-30 18:30:24 -0700

    Test against a_max = 1.0 too.

--- classylss/tests/test_against_astropy.py
+++ classylss/tests/test_against_astropy.py
@@ -10,59 +10,59 @@ c_flat = ac.FlatLambdaCDM(H0=70, Om0=0.3, Ob0=0.04, Tcmb0=2.7255)
 c_open = ac.LambdaCDM(H0=70, Om0=0.3, Ob0=0.04, Ode0=0.65, Tcmb0=2.7255)
 c_closed = ac.LambdaCDM(H0=70, Om0=0.3, Ob0=0.04, Ode0=0.85, Tcmb0=2.7255)
 
-@pytest.mark.parametrize("c2", [c_flat, c_open, c_closed])
-def test_against_astropy(c2):
+@pytest.mark.parametrize("c2, a_max", [
+             (c_flat, 1.0), (c_open, 1.0), (c_closed, 1.0),
+             (c_flat, 2.0), (c_open, 2.0), (c_closed, 2.0),
+            ])
+def test_against_astropy(c2, a_max):
+    if a_max > 1.0:
+        z = numpy.array([1.0, 0., -0.25])
+    else:
+        z = numpy.array([1.0, 0.])
 
     # classylss
-    pars = {'h':c2.h, 'Omega_cdm':c2.Odm0, 'Omega_b':c2.Ob0, 'Omega_k':c2.Ok0, 'T_cmb':c2.Tcmb0.value, 'a_max':2.0}
+    pars = {'h':c2.h, 'Omega_cdm':c2.Odm0, 'Omega_b':c2.Ob0, 'Omega_k':c2.Ok0, 'T_cmb':c2.Tcmb0.value, 'a_max':a_max}
     cosmo = ClassEngine(pars)
     ba = Background(cosmo)
 
     # age
-    z = numpy.array([1.0, 0., -0.25])
     this = ba.time(z)
     that = c2.age(z).value
     assert_allclose(this, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
-    # angular diameter distance
-    z = numpy.array([1.0, -0.25])
-    this = ba.angular_diameter_distance(z) # in Mpc/h
-    that = c2.angular_diameter_distance(z).value * c2.h
-    assert_allclose(this, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
-
     # comoving distance
-    z = numpy.array([1.0, -0.25])
     this = ba.comoving_distance(z) # in Mpc/h
     that = c2.comoving_distance(z).value * c2.h
     assert_allclose(this, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
+    # angular diameter distance
+    this = ba.angular_diameter_distance(z) # in Mpc/h
+    that = c2.angular_diameter_distance(z).value * c2.h
+    assert_allclose(this, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
+
+
     # comoving transverse distance
-    z = numpy.array([1.0, -0.25])
     this = ba.comoving_transverse_distance(z) # in Mpc/h
     that = c2.comoving_transverse_distance(z).value * c2.h
     assert_allclose(this, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # critical density
-    z = numpy.array([1.0, 0., -0.25])
     this = ba.rho_crit(z) # in 1e10 M_sun h^2 / Mpc^3
     that = c2.critical_density(z)
     that = that.to((units.Msun)/(units.Mpc)**3).value / c2.h**2 / 1e10
     assert_allclose(this, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # efunc
-    z = numpy.array([1.0, 0., -0.25])
     this = ba.efunc(z)
     that = c2.efunc(z)
     assert_allclose(this, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # luminosity distance
-    z = numpy.array([1.0, -0.25])
     this = ba.luminosity_distance(z) # in Mpc/h
     that = c2.luminosity_distance(z).value * c2.h
     assert_allclose(this, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # baryon omega
-    z = numpy.array([1.0, 0., -0.25])
     this1 = ba.Omega_b(z)
     this2 = ba.rho_b(z) / ba.rho_crit(z)
     that = c2.Ob(z)
@@ -70,7 +70,6 @@ def test_against_astropy(c2):
     assert_allclose(this2, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # lambda omega
-    z = numpy.array([1.0, 0., -0.25])
     this1 = ba.rho_lambda(z) / ba.rho_crit(z)
     this2 = ba.Omega_lambda(z)
     that = c2.Ode(z)
@@ -78,7 +77,6 @@ def test_against_astropy(c2):
     assert_allclose(this2, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # cdm omega
-    z = numpy.array([1.0, 0., -0.25])
     this1 = ba.rho_cdm(z) / ba.rho_crit(z)
     this2 = ba.Omega_cdm(z)
     that = c2.Odm(z)
@@ -86,7 +84,6 @@ def test_against_astropy(c2):
     assert_allclose(this2, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # photons omega
-    z = numpy.array([1.0, 0., -0.25])
     this1 = ba.rho_g(z) / ba.rho_crit(z)
     this2 = ba.Omega_g(z)
     that = c2.Ogamma(z)
@@ -94,7 +91,6 @@ def test_against_astropy(c2):
     assert_allclose(this2, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # total radiation omega
-    z = numpy.array([1.0, 0., -0.25])
     this1 = (ba.rho_g(z) + ba.rho_ur(z)) / ba.rho_crit(z)
     this2 = ba.Omega_g(z) + ba.Omega_ur(z)
     that = c2.Ogamma(z) + c2.Onu(z)
@@ -102,7 +98,6 @@ def test_against_astropy(c2):
     assert_allclose(this2, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # curvature omega
-    z = numpy.array([1.0, 0., -0.25])
     this1 = ba.rho_k(z) / ba.rho_crit(z)
     this2 = ba.Omega_k(z)
     that = c2.Ok(z)
@@ -110,7 +105,6 @@ def test_against_astropy(c2):
     assert_allclose(this2, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # matter omega
-    z = numpy.array([1.0, 0., -0.25])
     this1 = ba.rho_m(z) / ba.rho_crit(z)
     this2 = ba.Omega_m(z)
     that = c2.Om(z)
@@ -118,7 +112,6 @@ def test_against_astropy(c2):
     assert_allclose(this2, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
 
     # T cmb
-    z = numpy.array([1.0, 0., -0.25])
     this = ba.T_cmb(z)
     that = c2.Tcmb(z).value
     assert_allclose(this, that, rtol=1e-3, atol=1e-3, err_msg='z = %s' %z)
